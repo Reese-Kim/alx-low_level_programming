271730cc664000d21fa9154923ea4b003873ea40
Reese-Kim <canhamreese13@gmail.com>
2023-06-18 14:27:49 -0700

    attempt 7 of 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet.
- * Return: Always 0 (Success)
+ * main - main function
+ *
+ * Return: always 0
  */
+
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	char i:
 
-	for (i = 0; i < 26; i++)
-	{	
-		putchar(alp[i]);
-	}
-	putchar('\n');
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n')
 	return (0);
 }
