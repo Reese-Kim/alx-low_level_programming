6a634320eac937e35bcaeefdd087ab3537655e5f
Reese-Kim <canhamreese13@gmail.com>
2023-08-07 12:43:54 -0700

    my second file io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,44 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-/**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
- *
- * Return: If the function fails - -1.
- *         Otherwise - 1.
- */
-int create_file(const char *filename, char *text_content)
-{
-	int fd, w, len = 0;
+int create_file(const char *filename, char *text_content) {
+    if (filename == NULL) {
+        return -1; // Return -1 if filename is NULL
+    }
 
-	if (filename == NULL)
-		return (-1);
+    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (fd == -1) {
+        perror("Error opening file");
+        return -1;
+    }
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
+    if (text_content != NULL) {
+        ssize_t write_result = write(fd, text_content, strlen(text_content));
+        if (write_result == -1) {
+            perror("Error writing to file");
+            close(fd);
+            return -1;
+        }
+    }
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
+    close(fd);
+    return 1;
+}
 
-	if (fd == -1 || w == -1)
-		return (-1);
+int main() {
+    const char *filename = "example.txt";
+    char *text_content = "Hello, this is some text content.";
 
-	close(fd);
+    int result = create_file(filename, text_content);
+    if (result == 1) {
+        printf("File created successfully.\n");
+    } else if (result == -1) {
+        printf("File creation failed.\n");
+    }
 
-	return (1);
+    return 0;
 }
